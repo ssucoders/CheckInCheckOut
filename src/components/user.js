bb9a7a9303006eae61c99e2d20a538362bb9df9e
mUbaidAwan <uawan2385@gmail.com>
2019-04-24 00:05:24 +0500

    added a check if user exists then show ui

--- src/components/user.js
+++ src/components/user.js
@@ -18,21 +18,23 @@ checkOutUser(){
     render(){
         let user = this.props.item;
         return (
-<div>
-    <div className="card">
-        <img src={"/images/users/"+ user.picture} className="card-img-top" alt="..." />
-        <div className="card-body">
-            <h4 className="card-title">{user.name}</h4>
-            <h5 className="card-text">{user.designation}</h5>
-            <p>Check In Time: {user.checkin!=null? user.checkin.toLocaleTimeString():""}</p> 
-            <p>Check Out Time: {user.checkout!=null? user.checkout.toLocaleTimeString():""}</p>
-            <a href="#" className="btn btn-primary" onClick={this.checkInUser}>Check In</a>
-            {
-                user.checkin!=null?<button className="btn btn-danger" onClick={this.checkOutUser}>Check Out</button>:""
-            }
-        </div>
-    </div>
- </div>
+            <div>
+                {user?
+                <div className="card">
+                    <img src={"/images/users/"+ user.picture} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h4 className="card-title">{user.name}</h4>
+                        <h5 className="card-text">{user.designation}</h5>
+                        <p>Check In Time: {user.checkin!=null? user.checkin.toLocaleTimeString():""}</p> 
+                        <p>Check Out Time: {user.checkout!=null? user.checkout.toLocaleTimeString():""}</p>
+                        <a href="#" className="btn btn-primary" onClick={this.checkInUser}>Check In</a>
+                        {
+                            user.checkin!=null?<button className="btn btn-danger" onClick={this.checkOutUser}>Check Out</button>:""
+                        }
+                    </div>
+                </div>
+            :null}
+            </div>
             
         )
     }
