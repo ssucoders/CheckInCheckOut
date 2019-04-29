769fe182c6c21eb131be04d3848401897007fbbd
mUbaidAwan <uawan2385@gmail.com>
2019-04-29 20:17:04 +0500

    fixed navbar

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,7 @@ class Header extends Component{
 
     render(){
         return (
-        <nav className="navbar-fixed-top navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <a className="navbar-brand" href="#">SSU</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
